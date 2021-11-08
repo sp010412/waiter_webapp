9ae4774fbbbd4c7340d8de55c09c67b0cb11762d
sp010412 <smajwede7@gmail.com>
2021-11-08 21:18:47 +0200

    layout

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+
 //const flash = require('express-flash');
 //const session = require('express-session');
 const waiter = require("./waiter-webapp");
@@ -37,6 +38,7 @@ const pool = new Pool({
 app.engine('handlebars', handlebarSetup);
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
+
 
 // app.use(session({
 //     secret: 'keyboard cat',
